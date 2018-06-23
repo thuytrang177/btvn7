9652fb928068f3fa50673d5be064add3a90bfe97
Nguyen Thuy Trang <nthuytrang1788@gmail.com>
2018-06-23 11:32:47 +0700

    update 2.0

--- person.js
+++ person.js
@@ -2,7 +2,7 @@
 import { user } from './App';
 
 export const checkAge = (age) => {
-    if (age > 18) {
+    if (age >= 18) {
         age = "18 or more";
     } else {
         age = "less than 18"
